c4966c0c74359fe68fdda1402f88d36c6b9035c6
Shaun Gill <sgill@wildlifetrusts.org>
2016-12-01 16:57:53 +0000

    fuckup

--- js/jquery.tubular.1.0.js
+++ js/jquery.tubular.1.0.js
@@ -28,6 +28,7 @@
         volumeUpClass: 'tubular-volume-up',
         volumeDownClass: 'tubular-volume-down',
         increaseVolumeBy: 10,
+		type: 'div',
         start: 0
     };
 
@@ -37,13 +38,18 @@
         var options = $.extend({}, defaults, options),
             $body = $('body') // cache body node
             $node = $(node); // cache wrapper node
-
-        // build container
+		
+			options.width = $(node).width();
+		if(options.type == 'div'){
+			console.log(this);
+		}
+		// build container
         var tubularContainer = '<div id="tubular-container" style="overflow: hidden; position: fixed; z-index: 1; width: 100%; height: 100%"><div id="tubular-player" style="position: absolute"></div></div><div id="tubular-shield" style="width: 100%; height: 100%; z-index: 2; position: absolute; left: 0; top: 0;"></div>';
 
         // set up css prereq's, inject tubular container and set up wrapper defaults
         $('html,body').css({'width': '100%', 'height': '100%'});
-        $body.prepend(tubularContainer);
+        //$body.prepend(tubularContainer);
+        $node.prepend(tubularContainer);
         $node.css({position: 'relative', 'z-index': options.wrapperZIndex});
 
         // set up iframe player, use global scope so YT api can talk
